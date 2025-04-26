3efcf32c07119ce92304c946144b2425029bac5c
Justin Wong <justinw@cloudflare.com>
2025-04-26 13:53:59 -0700

    250426 remove stale migration

--- db/migrate/20220207071750_remove_reference_from_comments.rb
+++ db/migrate/20220207071750_remove_reference_from_comments.rb
@@ -1,5 +1,5 @@
-class RemoveReferenceFromComments < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :comments, :message_id, true
-  end
-end
+# class RemoveReferenceFromComments < ActiveRecord::Migration[6.1]
+#   def change
+#     change_column_null :comments, :message_id, true
+#   end
+# end
